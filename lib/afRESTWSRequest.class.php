372edef4b1a45fa0144ec249548ce836f7c3b561
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2010-08-11 19:59:13 +0000

    #538 - Small changes

--- lib/afRESTWSRequest.class.php
+++ lib/afRESTWSRequest.class.php
@@ -7,12 +7,14 @@
 class afRESTWSRequest
 {
     private $parameters;
+    private $parametersEncoded;
     private $resourceId;
     private $httpMethod;
     
     function __construct($parameters, $resourceId, $httpMethod)
     {
         $this->parameters = $parameters;
+        $this->parametersEncoded = json_encode($parameters);
         $this->resourceId = $resourceId;
         $this->httpMethod = $httpMethod;
     }
@@ -20,6 +22,11 @@ class afRESTWSRequest
     function getParameters()
     {
         return $this->parameters;
+    }
+
+    function getParametersEncoded()
+    {
+        return $this->parametersEncoded;
     }
 
     function getResourceId()
@@ -31,5 +38,34 @@ class afRESTWSRequest
     {
         return $this->httpMethod;
     }
+
+    function getHeaders()
+    {
+        $headers = array('Content-Type: application/json');
+
+        if ($this->httpMethod == 'PUT') {
+            if (count($this->parameters) > 0) {
+                $contentLength = strlen($this->parametersEncoded);
+                $headers[] = "Content-Length: {$contentLength}";
+            } else {
+                $headers[] = "Content-Length: 0";
+            }
+        }
+        
+        return $headers;
+    }
+
+    function getBaseUrl()
+    {
+        return "";
+    }
+
+    function getUrl()
+    {
+        $baseUrl = $this->getBaseUrl();
+        $url = "{$baseUrl}{$this->resourceId}" . (substr(strrev($this->resourceId),0,1) != '/' ? '/' : '');
+        return $url;
+    }
+
 }
 ?>
